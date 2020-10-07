66334cbe6306e329a8160c7c52bff473b3830dbf
Kai T. Ohlhus <k.ohlhus@gmail.com>
2020-10-07 19:13:13 +0900

    buildbot download app

--- master/defaults/app/octave_download_app.py
+++ master/defaults/app/octave_download_app.py
@@ -10,7 +10,8 @@ from buildbot.process.results import statusToString
 
 octavedownloadapp = Flask(__name__,
                           root_path = os.path.dirname(__file__),
-                          template_folder = "")
+                          template_folder = "",
+                          static_folder = "")
 
 @octavedownloadapp.route("/index.html")
 def main():
@@ -22,6 +23,7 @@ def main():
   # Absolute directory, where stable Octave builds are stored.
   oct_stable_dir = "/buildbot/data/stable"
   oct_stable_url = octavedownloadapp.config["DATA_DIR_URL"] + "/data/stable"
+  oct_repo_url = octavedownloadapp.config["OCTAVE_HG_REPO_URL"]
 
   # Create list of directory names with time stamps.
   oct_build_id_dirs = []
@@ -31,14 +33,25 @@ def main():
         if entry.is_dir():
           oct_build_id_dirs.append({
             "name": entry.name,
+            "version": [],
             "sort": os.path.getctime(entry),
-            "date": []
+            "date": [],
+            "files": []
           })
           oct_build_id_dirs[-1]["date"] = time.strftime(
             "%a, %Y %b %d %H:%M:%S %z",
             time.gmtime(oct_build_id_dirs[-1]["sort"]))
   # Ordered by newest directory first.
   oct_build_id_dirs = sorted(oct_build_id_dirs, key = lambda i: i["sort"], reverse=True)
+
+  # Fetch list of files for each build
+  if os.path.isdir(oct_stable_dir):
+    for idx, entry in enumerate(oct_build_id_dirs):
+      files = os.listdir(oct_stable_dir + '/' + entry["name"])
+      version = files[[idx for idx, s in enumerate(files)
+                       if 'octave-' in s and '.tar.gz' in s][0]]
+      oct_build_id_dirs[idx]["files"] = files
+      oct_build_id_dirs[idx]["version"] = version[7:-7]  # octave-VERSION.tar.gz
 
   # Properties are actually not used in the template example, but this is
   # how you get more properties
@@ -52,4 +65,5 @@ def main():
                          builders = builders,
                          builds = builds,
                          oct_build_id_dirs = oct_build_id_dirs,
-                         oct_stable_url = oct_stable_url)
+                         oct_stable_url = oct_stable_url,
+                         oct_repo_url = oct_repo_url)
