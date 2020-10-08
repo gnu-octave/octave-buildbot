0eac0094de84295e8821375860909699439804a7
Kai T. Ohlhus <k.ohlhus@gmail.com>
2020-10-08 10:18:00 +0900

    master/defaults/tidy_up.sh: actually tidy up

--- master/defaults/tidy_up.sh
+++ master/defaults/tidy_up.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Helper script to tidy up a directory, e.g. `/buildbot/data`, if a threshold
-# given in MegaByte is exceeded.
+# Helper script to tidy up a directory, e.g. `/buildbot/data/stable`, if a
+# threshold given in MegaByte is exceeded.
 #
-# Usage:  ./tidy_up.sh 60000 /buildbot/data
+# Usage:  ./tidy_up.sh 60000 /buildbot/data/stable
 
 DATA_THRESHOLD=$1  # MegaByte
 BUILD_SIZE="10000" # MegaByte
@@ -17,6 +17,7 @@ echo "--> ${THRESHOLD} MB threshold. "
 if [[ "$(du -sm | awk '{print $1;}')" > "${THRESHOLD}" ]]; then
   OLDEST_DIR=$(ls -tr | head -n 1)
   echo "--> $(du -sm | awk '{print $1;}') MB is too much, remove '${OLDEST_DIR}'!"
+  rm -Rf ${OLDEST_DIR}
 fi
 
 echo "--> $(du -sm | awk '{print $1;}') MB now"
